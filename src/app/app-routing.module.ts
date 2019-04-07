271d7ac31b232c10adfb301dcdb3e7af4ab9cf9e
zeljko.grijak@adacco.com <zeljko.grijak@adacco.com>
2019-04-07 15:44:59 +0200

    Početna

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SiteLayoutComponent } from './_layout/site-layout/site-layout.component';
+import { SiteHeaderComponent } from './_layout/site-header/site-header.component';
+import { SiteFooterComponent } from './_layout/site-footer/site-footer.component';
+import { LoginBarComponent } from './_layout/login-bar/login-bar.component';
+import { HomeComponent } from './home/home.component';
+import { GlavnaComponent } from './glavna/glavna.component';
+
+const routes: Routes = [
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: 'glavna', component: GlavnaComponent }
+  {
+    path: '',
+    component: SiteLayoutComponent,
+    children: [
+      { path: '', component: GlavnaComponent, pathMatch: 'full'},
+      { path: 'glavna', component: GlavnaComponent },
+      { path: 'home', component: HomeComponent }
+
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteLayoutComponent } from './_layout/site-layout/site-layout.component';
+import { SiteHeaderComponent } from './_layout/site-header/site-header.component';
+import { SiteFooterComponent } from './_layout/site-footer/site-footer.component';
+import { LoginBarComponent } from './_layout/login-bar/login-bar.component';
+import { HomeComponent } from './home/home.component';
+import { GlavnaComponent } from './glavna/glavna.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteLayoutComponent,
+    SiteHeaderComponent,
+    SiteFooterComponent,
+    LoginBarComponent,
+    HomeComponent,
+    GlavnaComponent
   ],
   imports: [
     BrowserModule,

763589ab7d9e4a49acd18e9a71dba81cec123ab7
zeljko.grijak@adacco.com <zeljko.grijak@adacco.com>
2019-04-08 14:29:30 +0200

    Nakon instalacija svih servisa i jquery

--- src/app/_layout/site-header/site-header.component.ts
+++ src/app/_layout/site-header/site-header.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import {Component, ElementRef, Inject, OnInit, ViewChild} from '@angular/core';
+
+import { Subscription } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import {MessageComponentService} from '../../message.component.service';
+import {DbService} from '../../db.service';
+import {Dekl} from '../../dekl.service';
 
 @Component({
   selector: 'app-site-header',
@@ -6,10 +15,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./site-header.component.css']
 })
 export class SiteHeaderComponent implements OnInit {
+  doslaporuka: string = 'jos nista ...';
+  globv = environment;
 
-  constructor() { }
+  message: any;
+  subscription: Subscription;
+
+  public tsearchstring: string;
+  public sstudent_code: string;
+
+  constructor(
+    private router: Router,
+    private messageService: MessageComponentService,
+    public dekl: Dekl
+  ) {
+    // this.tsearchstring = '00';
+
+    this.subscription = this.messageService.getMessage().subscribe(
+      message => {
+        this.message = message;
+
+        let a = dajobjectkey(message.dekl, 'tekst3');
+
+        if ( this.globv.emit_to_page === 'header') {
+          // alert(a);
+          this.doslaporuka = 'Header je čuo! ' + a;
+        }
+      });
+
+  }
 
   ngOnInit() {
   }
 
+  public sajtsearch(): any {
+
+    //let c = tstr;
+
+    const a = this.tsearchstring;
+    const b = this.sstudent_code;
+
+    // if (this.tsearchstring === null || this.tsearchstring  === undefined || this.tsearchstring.length <= 2) {
+    if (this.tsearchstring === null || this.tsearchstring  === undefined) {
+      return;
+    } else {
+      // alert(this.tsearchstring);
+      //
+      this.globv.tsearchstring = this.tsearchstring;
+      this.sendMessage2();
+    }
+
+
+    this.router.navigateByUrl('/sajtsearch');
+  }
+
+  sendMessage2(): void {
+    // alert('Šaljem 3');
+    // this.dekl.tekst3 = 'Naredba from Header3 Component to Page3 Component!';
+    this.dekl.tekst3 = this.tsearchstring;
+    this.globv.emit_to_page = 'sitesearch';
+    // send message to subscribers via observable subject
+    this.messageService.sendMessage2(this.dekl);
+
+    this.tsearchstring  = '';
+  }
+
 }
+
+function dajobjectkey(tobject: object, tkey: string) {
+  let result = '';
+
+  for (let p in tobject) {
+    if ( tobject.hasOwnProperty(p) ) {
+      // result += p + ' , ' + message.dekl[p];
+      if (p === tkey) {result = tobject[p]; }
+    }
+  }
+  return result;
+}
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,45 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  globalVar1: 200,
+  globalVar2: 201,
+  PONISTI: true,
+  tselind: 0,
+  tscrollpos: 0,
+  tidstud: -1,
+  noviidstud: -1,
+  trowind: -1,
+  prethscrollpos: 0,
+  prethrowind: -1,
+  NOVI: false,
+  tlogged: true,
+  emit_to_page: '',
+  DEMO: false,
+  tIMEMDB: 'AdcUniversity',
+  tSlikeDir: 'UploadedImages',
+  tSlikaFile: 'Photo000.jpg',
+  YES: false,
+  moji_data: {},
+  HTTP_ANSWER: 0,
+  err_message: '',
+  DIALOGZATVOREN: false,
+  tscrollposition_top: 0,
+  gtstind: '0',
+  gtendind: '16',
+  toffset: 0,
+  tsearchstring: '',
+  //THOST: 'localhost',
+  THOST: 'www.adacco.com'
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
